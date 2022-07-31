a1aa2a5870f7689f682da5d918f9e5694d9c27fb
Your Name <you@example.com>
2022-07-31 18:49:16 -0400

    test

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const myPath = "./api/someinfo.json"
 
 let student = {"success": true, 'addList': []};
 if('addList' in student) {
@@ -6,6 +7,9 @@ if('addList' in student) {
 }
 let r = (Math.random() + 1).toString(36).substring(7);
 student['addList'].push(r)
-fs.writeFileSync('./api/someinfo.json', JSON.stringify(student))
+if (!fs.existsSync(myPath)){
+    fs.mkdirSync(myPath, { recursive: true });
+}
+fs.writeFileSync(myPath, JSON.stringify(student))
 
 console.log(student);
